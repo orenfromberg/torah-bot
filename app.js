ac040d451e576753a52880fa9231b50c12c4a32f
Oren <orenfromberg@gmail.com>
2016-09-16 00:12:59 -0400

    Now responds to comments that mention it.

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ var config = require('./config.json');
 // Our new instance associated with a single account.
 // It takes in various configuration options.
 var reddit = new Snoocore({
-  userAgent: '/u/username myApp@3.0.0', // unique string identifying the app
-  oauth: {
-    type: 'script',
-    key: config['key'], // OAuth client key (provided at reddit app)
-    secret: config['secret'], // OAuth secret (provided at reddit app)
-    username: config['username'], // Reddit username used to make the reddit app
-    password: config['password'], // Reddit password for the username
-    // The OAuth scopes that we need to make the calls that we 
-    // want. The reddit documentation will specify which scope
-    // is needed for evey call
-    scope: [ 'identity', 'read', 'vote', 'privatemessages' ]
-  }
+    userAgent: '/u/username myApp@3.0.0', // unique string identifying the app
+    oauth: {
+        type: 'script',
+        key: config['key'], // OAuth client key (provided at reddit app)
+        secret: config['secret'], // OAuth secret (provided at reddit app)
+        username: config['username'], // Reddit username used to make the reddit app
+        password: config['password'], // Reddit password for the username
+        // The OAuth scopes that we need to make the calls that we 
+        // want. The reddit documentation will specify which scope
+        // is needed for evey call
+        scope: ['identity', 'read', 'vote', 'privatemessages', 'submit']
+    }
 });
 
 // reddit('/message/unread').get().then(function(result) {
@@ -29,20 +29,66 @@ var reddit = new Snoocore({
 // Instead of an HTTP verb, use a Snoocore helper `.listing`
 // It will return a "slice" for a listing that has various 
 // helpers attached to it.
-// reddit('/r/askreddit/hot').listing().then(function(slice) {
-reddit('/message/unread').listing().then(function(slice) {
-  
-  // This is the front page of /r/askreddit
-  // `slice.children` contains the contents of the page.
-  console.log(slice.children);
-  
-  // Get a promise for the next slice in this 
-  // listing (the next page!)
-  return slice.next();
-  
-}).then(function(slice) {
-  
-  // This is the second page of /r/askreddit
-  console.log(slice.children);
-  
-});
+
+var numUpdates = 0;
+
+function markAsRead(message) {
+    console.log("marking comment as read");
+    reddit('/api/read_message').post({
+        id: message.data.name    
+    }).then(function () {
+        console.log("message " + message.data.name + " marked as read.")
+    });
+}
+
+function handleComment(comment) {
+    //
+    console.log("handling comment");
+
+    // respond to comment
+    reddit('/api/comment').post({
+        api_type: 'json',
+        text: 'Shalom aleichem, I am /u/TorahBot.',
+        thing_id: comment.data.name
+    }).then(markAsRead(comment)).catch(function(error) {
+        console.log("unable to respond: " + error)
+    });
+
+}
+
+function processChild(child) {
+    if (child.kind === 't1')
+        handleComment(child);
+    else // mark as read
+        markAsRead(child);
+}
+
+function getUnreadMessages() {
+    numUpdates++;
+    console.log("times updated: " + numUpdates);
+    reddit('/message/unread').listing().then(function (slice) {
+
+        // This is the front page of /r/askreddit
+        // `slice.children` contains the contents of the page.
+        console.log("processing first slice");
+        console.log(slice.children);
+
+        slice.children.forEach(child => processChild(child));
+
+        // Get a promise for the next slice in this 
+        // listing (the next page!)
+        // return slice.next();
+
+    })
+    // .then(function (slice) {
+
+    //     // This is the second page of /r/askreddit
+    //     console.log("second slice");
+    //     console.log(slice.children);
+    //     slice.children.forEach(child => processChild(child));
+
+    // });
+}
+
+getUnreadMessages();
+// setInterval(getUnreadMessages, 5000);
